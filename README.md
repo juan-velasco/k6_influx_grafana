2f8b722591d489fbc36b2e0d889dc4898aa65c12
Juan Velasco <jvelasco@iti.es>
2021-06-18 13:10:32 +0200

    Agregar configuraciones

--- README.md
+++ README.md
@@ -4,3 +4,11 @@
 
 ### Ejecutar los test con k6
 `docker-compose run k6 run /scripts/single-request.js`
+
+### Lanzar smoke test (un usuario / una iteración)
+Si indicamos los VUs y el número de iteraciones se sobreescriben las opciones por defecto. 
+También podemos utilizar un fichero de configuración (recomendado).
+
+`docker-compose run k6 run /scripts/single-request.js --vus 1 --iterations 1`
+
+`docker-compose run k6 run /scripts/single-request.js --config "/scripts/configs/smoke_test.json"`
--- scripts/single-request.js
+++ scripts/single-request.js
@@ -6,20 +6,6 @@ import { Counter } from 'k6/metrics';
 
 export const requests = new Counter('http_reqs');
 
-// you can specify stages of your test (ramp up/down patterns) through the options object
-// target is the number of VUs you are aiming for
-
-export const options = {
-  stages: [
-    { target: 20, duration: '1m' },
-    { target: 15, duration: '1m' },
-    { target: 0, duration: '1m' },
-  ],
-  thresholds: {
-    requests: ['count < 100'],
-  },
-};
-
 export default function () {
   // our HTTP request, note that we are saving the response to res, which can be accessed later
 
@@ -29,6 +15,5 @@ export default function () {
 
   const checkRes = check(res, {
     'status is 200': (r) => r.status === 200,
-    'response body': (r) => r.body.indexOf('Feel free to browse') !== -1,
   });
 }
